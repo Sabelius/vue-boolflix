95dcff6487396e7cf2bcba3b36f32c538de89df3
Sabelius <sbebby92@virgilio.it>
2021-10-04 13:09:50 +0200

    completamento estetica

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,17 +4,17 @@
       <div class="col-10">
         <img :src="require('../img/boolflix.png')" class="header-logo" />
       </div>
-      <div class="col-lg-2 col-md-4 col-sm-5 col-6 d-flex align-items-center">
+      <div class="col-lg-2 col-md-4 col-12 d-flex align-items-center">
         <input
           type="text"
-          placeholder="Movies and Series"
+          id="Movies and Series"
           v-model="searchQuery"
           @keydown.enter="$emit('search', searchQuery)"
         />
         <span class="p-2">
           <button
             type="button"
-            class="btn btn-primary "
+            class="btn btn-primary"
             @click="$emit('search', searchQuery)"
           >
             <i class="fas fa-search"></i>
@@ -46,8 +46,8 @@ div {
 .header-logo {
   width: 200px;
 }
-input{
+input {
   height: 30px;
+  width: 200px;
 }
-
 </style>
--- src/components/card.vue
+++ src/components/card.vue
@@ -1,60 +1,106 @@
 <template>
-  <section>
+  <div class="background">
+    <div class="container">
+      <div class="row pt-5">
+        <!-- film -->
+        <div
+          v-for="film in films"
+          :key="film.id"
+          class="col-lg-4 col-md-6 col-12 p-3"
+        >
+          <div>
+            <img class="poster img-fluid" :src="posterFunction(film)" />
+            <div class="title pt-2">
+              <strong>Titolo: </strong>
+              <span class="description">{{ film.title }}</span>
+            </div>
+            <div class="original-title">
+              <strong>Original Title: </strong>
+              <span class="description">{{ film.original_title }}</span>
+            </div>
+            <div class="language">
+              <span
+                class="description"
+                v-if="flags.includes(film.original_language)"
+              >
+                <strong>Language: </strong
+                ><img
+                  :src="require('../img/' + film.original_language + '.png')"
+                  class="flag"
+              /></span>
+              <span v-else
+                ><strong>Language: </strong>{{ film.original_language }}</span
+              >
+            </div>
+            <div class="vote">
+              <strong>Voto: </strong>
+              <span class="description">
+                <i
+                  v-for="realfilmstar in getFloatInt(film.vote_average)"
+                  :key="realfilmstar"
+                  class="fas fa-star"
+                ></i>
+                <i
+                  v-for="maxfilmstar in 5 - getFloatInt(film.vote_average)"
+                  :key="maxfilmstar"
+                  class="far fa-star"
+                ></i>
+              </span>
+            </div>
+          </div>
+        </div>
 
-    <!-- film -->
-    <div v-for="film in films" :key="film.id" class="pt-3">
-      <img class="poster" :src="posterFunction(film)" />
-      <div class="title pt-2">
-        <strong>Titolo: </strong>
-        <span class="description">{{ film.title }}</span>
-      </div>
-      <div class="original-title">
-        <strong>Original Title: </strong>
-        <span class="description">{{ film.original_title }}</span>
-      </div>
-      <div class="language">
-        <span class="description" v-if="flags.includes(film.original_language)">
-        <strong>Language: </strong><img  :src="require('../img/' + film.original_language + '.png')" class="flag"/></span>
-        <span v-else><strong>Language: </strong>{{ film.original_language }}</span>
-      </div>
-      <div class="vote">
-        <strong>Voto: </strong>
-        <span class="description">
-          <i v-for="realfilmstar in getFloatInt(film.vote_average)" :key="realfilmstar" class="fas fa-star"></i>
-          <i v-for="maxfilmstar in (5 - getFloatInt(film.vote_average))" :key="maxfilmstar" class="far fa-star"></i>
-        </span>
-      </div>
-    </div>
-    
-    <!-- serie tv -->
-    <div v-for="show in series" :key="show.id" class="pt-3">
-      <img class="poster" :src="posterFunction(show)" />
-      <div class="title pt-2">
-        <strong>Titolo: </strong>
-        <span class="description">{{ show.name }}</span>
-      </div>
-      <div class="original-title">
-        <strong>Original Title: </strong>
-        <span class="description">{{ show.original_name }}</span>
-      </div>
-      <div class="language">
-        <span class="description" v-if="flags.includes(show.original_language)">
-        <strong>Language: </strong><img  :src="require('../img/' + show.original_language + '.png')" class="flag"/></span>
-        <span v-else><strong>Language: </strong>{{ show.original_language }}</span>
-      </div>
-      <div class="vote">
-        <strong>Voto: </strong>
-        <span class="description">
-          <i v-for="realshowstar in getFloatInt(show.vote_average)" :key="realshowstar" class="fas fa-star"></i>
-          <i v-for="maxshowstar in (5 - getFloatInt(show.vote_average))" :key="maxshowstar" class="far fa-star"></i>
-          </span>
+        <!-- serie tv -->
+        <div
+          v-for="show in series"
+          :key="show.id"
+          class="col-lg-4 col-md-6 col-12 p-3"
+        >
+          <img class="poster img-fluid" :src="posterFunction(show)" />
+          <div class="title pt-2">
+            <strong>Titolo: </strong>
+            <span class="description">{{ show.name }}</span>
+          </div>
+          <div class="original-title">
+            <strong>Original Title: </strong>
+            <span class="description">{{ show.original_name }}</span>
+          </div>
+          <div class="language">
+            <span
+              class="description"
+              v-if="flags.includes(show.original_language)"
+            >
+              <strong>Language: </strong
+              ><img
+                :src="require('../img/' + show.original_language + '.png')"
+                class="flag"
+            /></span>
+            <span v-else
+              ><strong>Language: </strong>{{ show.original_language }}</span
+            >
+          </div>
+          <div class="vote">
+            <strong>Voto: </strong>
+            <span class="description">
+              <i
+                v-for="realshowstar in getFloatInt(show.vote_average)"
+                :key="realshowstar"
+                class="fas fa-star"
+              ></i>
+              <i
+                v-for="maxshowstar in 5 - getFloatInt(show.vote_average)"
+                :key="maxshowstar"
+                class="far fa-star"
+              ></i>
+            </span>
+          </div>
+        </div>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: "card",
   props: ["films", "series"],
@@ -65,21 +111,18 @@ export default {
   },
 
   methods: {
-
     // funzione che trova la locandina se questa esiste
     posterFunction(element) {
       if (element.poster_path != null) {
-        return (
-          "https://image.tmdb.org/t/p/w342" +
-          element.poster_path
-        );
-      } return require('../img/no-image.png');
+        return "https://image.tmdb.org/t/p/w342" + element.poster_path;
+      }
+      return require("../img/no-image.png");
     },
 
-    // funzione per rendere il voto un intero 
-     getFloatInt(element){
-       return Math.floor(element / 2);
-     }
+    // funzione per rendere il voto un intero
+    getFloatInt(element) {
+      return Math.floor(element / 2);
+    },
   },
 };
 </script>
@@ -87,11 +130,18 @@ export default {
 <style lang="scss" scoped>
 @import "../style/variables.scss";
 
+.background {
+  background-color: $card-bg;
+  min-height: calc(100vh - 66px);
+}
+.poster {
+  height: 512px;
+  width: 342px;
+}
 .flag {
   width: 30px;
 }
-.fa-star{
+.fa-star {
   color: $color-star;
 }
-
 </style>
